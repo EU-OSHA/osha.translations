3b2908c4194bcc417bc5016682110687069ccb96
Wolfgang Thomas <thomas@syslab.com>
2012-02-14 10:07:28 +0000

    Back to development: 1.0.11

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0.10'
+version = '1.0.11dev'
 
 long_description = (
     read('README.txt')
