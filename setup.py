8561b332b884b285f2812b1ff9c955a2a25b4f65
Wolfgang Thomas <thomas@syslab.com>
2012-08-17 08:46:19 +0000

    Preparing release 1.0.21

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '1.0.21dev'
+version = '1.0.21'
 
 long_description = (
     read('README.txt')
