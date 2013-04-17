d8cdc72e5b55a760fdeebfe9b960f770f48e6dce
Wolfgang Thomas <thomas@syslab.com>
2013-04-17 11:23:22 +0200

    Back to development: 2.0.8

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '2.0.7'
+version = '2.0.8.dev0'
 
 long_description = (
     read('README.txt')
