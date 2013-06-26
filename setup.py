867de90500bb6d31e0ff37407b9bacfce3940aa8
Wolfgang Thomas <thomas@syslab.com>
2013-06-26 18:17:22 +0200

    Preparing release 2.0.10

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import os
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
-version = '2.0.10.dev0'
+version = '2.0.10'
 
 long_description = (
     read('README.txt')
